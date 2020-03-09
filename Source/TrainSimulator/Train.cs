821a3644636b6ff0347d8da3362e2a1db200af21
Samuel Björkqvist <samuel.bjorkqvist@gmail.com>
2020-03-09 15:29:05 +0100

    @sammycastaway

---
+++ Source/TrainSimulator/Train.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TrainSimulator
+{
+    class Train
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaxSpeed { get; set; }
+        public bool Operated { get; set; }
+
+        public Train LoadTrain()
+        {
+         string[] TrainFilePath = File.ReadAllLines("trains.txt");
+
+            foreach (string line in TrainFilePath)
+            {
+                string[] parts = line.Split(',');
+                int id = int.Parse(parts[0]);
+                
+            }
+            return this;
+        }
+    }
+}
